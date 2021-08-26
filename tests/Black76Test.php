578e6f95747ca529c855dde025592fbce718975a
Zois Pagoulatos <zpagoulatos@hotmail.com>
2021-08-26 10:50:01 +0200

    Update test cases to reflect the timeToMaturity calculation

--- tests/Black76Test.php
+++ tests/Black76Test.php
@@ -4,41 +4,41 @@ use Kyos\OptionsCalculator\Black76;
 
 // Prices & Greeks
 it('calculates greeks for CALL options', function () {
-    $this->expect((new Black76())->getValues(Black76::CALL, 10.5, 12, 0.082, 0.60))->toBe([
-        'value' => 0.2405826183655344,
-        'delta' => 0.24449431791580983,
-        'gamma' => 0.17399585222314845,
-        'vega' => 0.009438057012140243,
-        'theta' => -3.450541861184725,
-        'rho' => -0.00019727774705973822,
+    $this->expect((new Black76())->getValues(Black76::CALL, 10.5, 12, 30/365.25, 0.60))->toBe([
+        'value' => 0.24105017253515562,
+        'delta' => 0.24471766261590738,
+        'gamma' => 0.17393760473172346,
+        'vega' => 0.009450490803288298,
+        'theta' => -3.4493812641756993,
+        'rho' => -0.0001979878213841114,
     ]);
 });
 
 it('calculates greeks for PUT options', function () {
-    $this->expect((new Black76())->getValues(Black76::PUT, 10.5, 12, 0.082, 0.60))->toBe([
-        'value' => 1.7393531225277215,
-        'delta' => -0.7546860181923143,
-        'gamma' => 0.17399585222314845,
-        'vega' => 0.009438057012140243,
-        'theta' => -3.435554156143103,
-        'rho' => -0.0014262695604727318,
+    $this->expect((new Black76())->getValues(Black76::PUT, 10.5, 12, 30/365.25, 0.60))->toBe([
+        'value' => 1.7398186455107651,
+        'delta' => -0.7544613193678329,
+        'gamma' => 0.17393760473172346,
+        'vega' => 0.009450490803288298,
+        'theta' => -3.4343935794459433,
+        'rho' => -0.0014290091544236264,
     ]);
 });
 
 // Implied volatilities
 it('calculates implied volatility for CALL options when we know the value', function () {
-    $this->expect((new Black76())->getImpliedVolatility(Black76::CALL, 10.5, 12, 0.082, 0.2405826183655344))->toBe(0.60);
+    $this->expect((new Black76())->getImpliedVolatility(Black76::CALL, 10.5, 12, 30/365.25, 0.24105017253515562))->toBe(0.60);
 });
 
 it('calculates implied volatility for PUT options when we know the value', function () {
-    $this->expect((new Black76())->getImpliedVolatility(Black76::PUT, 10.5, 12, 0.082, 1.7393531225277215))->toBe(0.60);
+    $this->expect((new Black76())->getImpliedVolatility(Black76::PUT, 10.5, 12, 30/365.25, 1.7398186455107651))->toBe(0.60);
 });
 
 // Expections
 it('throws expection if we try to derive implied volatility using newton rapshon', function () {
-    (new Black76())->getImpliedVolatility(Black76::PUT, 10.5, 12, 0.082, 0.240, Black76::METHOD_NEWTON_RAPHSON);
+    (new Black76())->getImpliedVolatility(Black76::PUT, 10.5, 12, 30/365.25, 0.240, Black76::METHOD_NEWTON_RAPHSON);
 })->throws('Not implemented');
 
 it('throws expection if we try to derive implied volatility using wrong method', function () {
-    (new Black76())->getImpliedVolatility(Black76::PUT, 10.5, 12, 0.082, 0.240, 99);
+    (new Black76())->getImpliedVolatility(Black76::PUT, 10.5, 12, 30/365.25, 0.240, 99);
 })->throws('Wrong method 99 or not supported');
